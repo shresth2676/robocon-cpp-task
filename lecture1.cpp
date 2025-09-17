11bee4ade929c0484241fc1ecc822b52851148ab
shresth2676 <shresth2676@gmail.com>
2025-09-17 23:31:48 +0530

    Add files via upload

---
+++ lecture1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    cout << "Shresth Chakraborty"<<endl;
+    cout <<"Enter age"<<endl;
+    int n;
+    cin >> n;
+    if (n>=18){
+        cout<<"Eligible to vote";
+    }
+    else{
+        cout<<"Note eligible to vote";
+    }
+}
